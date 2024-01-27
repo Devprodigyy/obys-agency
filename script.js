9244c14e208c40ed87db174269001c0d0a1a4d60
saarthack <ssarthak2003@outlook.com>
2024-01-27 16:22:37 +0530

    added gooey effect and completed page3

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ function loadingAnimation() {
             grow++
             timer.innerHTML = grow
         }
-    }, 29)
+    }, 20)
 
     setTimeout(function () {
         clearInterval(int)
@@ -49,18 +49,18 @@ function page2Animation() {
 
     var videoC = document.querySelector("#video-container")
     videoC.addEventListener("mouseenter", function () {
-        gsap.to(".mousefollower",{
-            opacity:0
+        gsap.to(".mousefollower", {
+            opacity: 0
         })
     })
     videoC.addEventListener("mouseleave", function () {
-        gsap.to(".mousefollower",{
-            opacity:1
+        gsap.to(".mousefollower", {
+            opacity: 1
         })
 
-        gsap.to("#play-btn",{
-            left:"70%",
-            top:"-15%"
+        gsap.to("#play-btn", {
+            left: "70%",
+            top: "-15%"
         })
     })
 
@@ -69,37 +69,37 @@ function page2Animation() {
 
     var flag = 0
 
-    videoC.addEventListener("click",function(){
-        if(flag == 0){
+    videoC.addEventListener("click", function () {
+        if (flag == 0) {
 
-            gsap.to(videoVideo,{
-                opacity:1
+            gsap.to(videoVideo, {
+                opacity: 1
             })
 
-            gsap.to("#play-btn",{
-                scale:0.8
+            gsap.to("#play-btn", {
+                scale: 0.8
             })
             document.querySelector("#play-btn").innerHTML = '<i class="ri-pause-line"></i>'
             videoVideo.play()
             flag = 1
-        }else{
-            gsap.to(videoVideo,{
-                opacity:0
+        } else {
+            gsap.to(videoVideo, {
+                opacity: 0
             })
-            gsap.to("#play-btn",{
-                scale:1
+            gsap.to("#play-btn", {
+                scale: 1
             })
             document.querySelector("#play-btn").innerHTML = '<i class="ri-play-fill"></i>'
-           
+
             videoVideo.pause()
             flag = 0
         }
     })
 
-    videoC.addEventListener("mousemove",function(dets){
-        gsap.to("#play-btn",{
-            left:dets.x - 555,
-            top:dets.y-200
+    videoC.addEventListener("mousemove", function (dets) {
+        gsap.to("#play-btn", {
+            left: dets.x - 555,
+            top: dets.y - 200
         })
     })
 }
@@ -107,5 +107,19 @@ function page2Animation() {
 page2Animation()
 
 
-Shery.mouseFollower()
+function SheryAnimation(){
+    Shery.mouseFollower()
 
+    Shery.makeMagnet("#nav h4")
+    
+    Shery.imageEffect(".images",{
+        style:6,
+        // debug:true,
+        gooey:true,
+        config:{"noiseDetail":{"value":6.11,"range":[0,100]},"distortionAmount":{"value":2.9,"range":[0,10]},"scale":{"value":59.54,"range":[0,100]},"speed":{"value":0.58,"range":[0,1]},"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":0.8333333134651184},"ignoreShapeAspect":{"value":true},"shapePosition":{"value":{"x":0,"y":0}},"shapeScale":{"value":{"x":0.5,"y":0.5}},"shapeEdgeSoftness":{"value":0,"range":[0,0.5]},"shapeRadius":{"value":0,"range":[0,2]},"currentScroll":{"value":0},"scrollLerp":{"value":0.07},"gooey":{"value":true},"infiniteGooey":{"value":false},"growSize":{"value":4,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1.5,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":true},"maskVal":{"value":1.27,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":true},"onMouse":{"value":0},"noise_speed":{"value":0.84,"range":[0,10]},"metaball":{"value":0.44,"range":[0,2]},"discard_threshold":{"value":0.5,"range":[0,1]},"antialias_threshold":{"value":0,"range":[0,0.1]},"noise_height":{"value":0.38,"range":[0,2]},"noise_scale":{"value":8.4,"range":[0,100]}}
+    })
+
+}
+
+
+SheryAnimation()
